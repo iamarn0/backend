aae67cf857ec8948df18f223a6020d2a42bda1d7
Pratyush Mondal <iamarno936@gmail.com>
2024-12-20 16:31:11 +0530

    utils code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,33 @@
+//promises method
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error));  
+    }
+};
+
+export { asyncHandler }
+
+
+
+
+//const asyncHandler = () => {};
+//const asyncHandler = (func) => {() => {}};
+//const asyncHandler = (func) => async() => {}; Higher order function(can take function as arguments)
+
+
+/*
+try-catch method
+
+
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next);
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+*/
