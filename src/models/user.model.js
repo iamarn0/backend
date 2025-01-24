70892bdced3789db90ffdc5f97b19c3fc0c3160c
Pratyush Mondal <iamarno936@gmail.com>
2025-01-24 23:55:22 +0530

    add registerd controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -54,7 +54,7 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function(next){
     if(this.isModified("password")){
-        this.password = bcrypt.hash(this.password, 10)
+        this.password = await bcrypt.hash(this.password, 10)
         next() 
     }
     else{next()}
@@ -64,21 +64,21 @@ userSchema.methods.isPasswordCorrect = async function(password) {
     return await bcrypt.compare(password,this.password)
 }
 
-userSchema.methods.generateAccessToken = funtion(){
+userSchema.methods.generateAccessToken = function(){
     return jwt.sign(
         {
-            _id:this._id,
-            email:this.email,
-            userName:this.userName,
-            fullName:this.fullName
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
-            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
         }
     )
 }
-userSchema.methods.generateRefreshToken = funtion(){
+userSchema.methods.generateRefreshToken = function(){
     return jwt.sign(
         {
             _id:this._id
